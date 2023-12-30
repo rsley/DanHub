422aaf1341b0d25b2f509d84188bbc0e92e79f23
Rafael Soley R. <rafaelsoley.multiple@gmail.com>
2023-12-30 14:29:50 +0000

    DarkDex Addition - Finished Main Loader (1)

--- mainscript.lua
+++ mainscript.lua
@@ -1,9 +1,34 @@
 local Library = loadstring(game:HttpGet"https://raw.githubusercontent.com/rsley/DanLib/main/file.lua")()
 local Window = Library.CreateLib("DanHub", "Sentinel")
-local Tab = Window:NewTab("Global")
+local GlobalTab = Window:NewTab("Global")
 
-local Section = Tab:NewSection("Main")
-Section:NewLabel("Made by Dan")
-Section:NewButton("Destroy Gui", "Destroys the gui", function()
-    game:GetService("CoreGui").DanHub:Destroy()
+local darkDex = false
+
+local GlobalMain = GlobalTab:NewSection("Made by IDEalistic")
+GlobalMain:NewButton("Destroy Gui", "Destroys the gui", function()
+    for i,v in pairs(game.CoreGui:GetDescendants()) do
+        if v.Name == "pages" then
+            v.Parent:Destroy()
+        end
+    end
+end)
+GlobalMain:NewKeybind("Toggle Gui", "Toggles the gui", Enum.KeyCode.RightShift, function()
+    Library:ToggleUI()
+end)
+GlobalMain:NewToggle("Dark Dex", "Makes Dex Explorer dark (before running it)", function(state)
+    if state then
+        darkDex = true
+    else
+        darkDex = false
+    end
+end)
+GlobalMain:NewButton("Dex Explorer", "Opens Dex Explorer", function()
+    if(darkDex) then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/rsley/DanHub/main/dexdark.lua"))()
+    else
+        loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Dex%20Explorer.txt"))()   
+    end
+end)
+GlobalMain:NewButton("RemoteSpy", "Spy for remote events", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
 end)
