df67bab52de73f9212ce412d932d1fbef422e7cb
Rafael Soley R. <rafaelsoley.multiple@gmail.com>
2023-12-31 05:09:14 +0000

    Added simple silent aim

--- newmainscript.lua
+++ newmainscript.lua
@@ -15,18 +15,21 @@ local Noclip = nil
 local Clip = nil
 local player = game.Players.LocalPlayer
 local Aimbot = {
-  Enabled = true,
+  Enabled = false,
   TeamCheck = true,
   AimPart = "Torso",
   Sensitivity = 0,
   CircleSides = 64,
   CircleColor = Color3.fromRGB(255, 255, 255),
   CircleTransparency = 0.7,
-  CircleRadius = 80,
+  CircleRadius = 120,
   CircleFilled = false,
   CircleVisible = true,
   CircleThickness = 0
 }
+local gmt = getrawmetatable(game)
+setreadonly(gmt, false)
+local oldNamecall = gmt.__namecall
 
 -- // Services \\ --
 local Players = game:GetService("Players")
@@ -294,6 +297,57 @@ function GetClosestPlayer()
   return Target
 end
 
+function notBehindWall(target)
+  local ray = Ray.new(Camera.CFrame.Position, (target.Position - Camera.CFrame.Position).unit * 300)
+  local part, position = workspace:FindPartOnRayWithIgnoreList(ray, {player.Character, Camera})
+  if part then
+    local humanoid = part.Parent:FindFirstChildOfClass("Humanoid")
+    if not humanoid then
+      humanoid = part.Parent.Parent:FindFirstChildOfClass("Humanoid")
+    end
+    if humanoid and target and humanoid.Parent == target.Parent then
+      local pos, visible = camera:WorldToScreenPoint(target.Position)
+      if visible then
+        return true
+      end
+    end
+  end
+end
+
+function playerClosestToMouse()
+  local target = nil
+  local maxDist = 100
+
+  function getPlayerClosestToMouse()
+    local target = nil
+    local maxDist = 100
+    for _,v in pairs(plrs:GetPlayers()) do
+        if v.Character then
+            if v.Character:FindFirstChild("Humanoid") and v.Character.Humanoid.Health ~= 0 and v.Character:FindFirstChild("HumanoidRootPart") and v.TeamColor ~= plr.TeamColor then
+                local pos, vis = camera:WorldToViewportPoint(v.Character.HumanoidRootPart.Position)
+                local dist = (Vector2.new(mouse.X, mouse.Y) - Vector2.new(pos.X, pos.Y)).magnitude
+                if dist < maxDist and vis then
+                    local torsoPos = camera:WorldToViewportPoint(v.Character.HumanoidRootPart.Position)
+                    local torsoDist = (Vector2.new(mouse.X, mouse.Y) - Vector2.new(torsoPos.X, torsoPos.Y)).magnitude
+                    local headPos = camera:WorldToViewportPoint(v.Character.Head.Position)
+                    local headDist = (Vector2.new(mouse.X, mouse.Y) - Vector2.new(headPos.X, headPos.Y)).magnitude
+                    if torsoDist > headDist then
+                        if notBehindWall(v.Character.Head) then
+                            target = v.Character.Head
+                        end
+                    else
+                        if notBehindWall(v.Character.HumanoidRootPart) then
+                            target = v.Character.HumanoidRootPart
+                        end
+                    end
+                    maxDist = dist
+                end
+            end
+        end
+    end
+    return target
+  end
+end
 -- // Main \\ --
 local GUI = Lib:Create{
   Name = "DanHub",
@@ -534,6 +588,27 @@ Combat:Toggle{
     Aimbot.Enabled = state
   end
 }
+Combat:Toggle{
+  Name = "Silent Aim",
+  StartingState = false,
+  Description = "Aims at the closest player without moving your camera",
+  Callback = function(state)
+    if state then
+      gmt.__namecall = newcclosure(function(self, ...)
+        local Args = {...}
+        local method = getnamecallmethod()
+        if tostring(self) == "HitPart" and tostring(method) == "FireServer" then
+            Args[1] = getPlayerClosestToMouse()
+            Args[2] = getPlayerClosestToMouse().Position
+            return self.FireServer(self, unpack(Args))
+        end
+        return oldNamecall(self, ...)
+      end)
+    else
+      gmt.__namecall = oldNamecall
+    end
+  end
+}
 Combat:ColorPicker{
   Style = Lib.ColorPickerStyles.Legacy,
   Callback = function(color)
@@ -542,7 +617,7 @@ Combat:ColorPicker{
 }
 Combat:Toggle{
   Name = "Team Check",
-  StartingState = false,
+  StartingState = true,
   Description = "Checks if the player is on your team",
   Callback = function(state)
     Aimbot.TeamCheck = state
